e7c55300df29b404789eb6f8c4adaf526cf237b4
Christopher J. McClellan <ckuhn203@gmail.com>
2017-07-08 17:09:25 -0400

    Remove cruft

--- src/gitnstats/Program.cs
+++ src/gitnstats/Program.cs
@@ -15,13 +15,6 @@ namespace GitNStats
             //TODO: handle non-existant repo exception
             using (var repo = new Repository(@"/Users/rubberduck/Documents/Source/theupsyde/"))
             {
-                // foreach (var commit in repo.Commits.Take(10))
-                // {
-                //     PrintCommitTreeInfo(commit);
-                // }
-
-                // repo.Head.Tip.Walk(repo, PrintTreeDiffs);
-
                 var changeCounts = new ConcurrentDictionary<String, int>();
 
                 var visitor = new CommitVisitor();
@@ -47,44 +40,6 @@ namespace GitNStats
                 {
                     Console.WriteLine($"{count.Value}\t{count.Key}");
                 }
-            }
-        }
-
-        private static void PrintTreeDiffs(Repository repo, Commit commit)
-        {
-            PrintCommitInfo(commit);
-            foreach (var parent in commit.Parents)
-            {
-                var diff = repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
-
-                foreach (var changed in diff)
-                {
-                    Console.Write(changed.Path + "\t");
-                    Console.Write(changed.Mode + "\t");
-                    Console.WriteLine(changed.Status);
-                }
-            }
-        }
-
-        private static void PrintCommitTreeInfo(Commit commit)
-        {
-            PrintCommitInfo(commit);
-            PrintFilePathsAt(commit);
-        }
-
-        private static void PrintCommitInfo(Commit commit)
-        {
-            Console.Write(commit.Sha + "\t");
-            Console.WriteLine(commit.MessageShort);
-        }
-
-        private static void PrintFilePathsAt(Commit commit)
-        {
-            foreach (var treeEntry in commit.Tree.Flatten())
-            {
-                Console.Write("\t");
-                Console.Write(treeEntry.Path + "\t");
-                Console.WriteLine(treeEntry.TargetType.ToString() + "\t");
             }
         }
     }
