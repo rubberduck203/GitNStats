973d218cfaeb2341d7c2cbf6de56df871a198614
Christopher J. McClellan <ckuhn203@gmail.com>
2017-07-08 00:27:02 -0400

    Print the working directory as of a given commit

--- src/gitnstats/Program.cs
+++ src/gitnstats/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using LibGit2Sharp;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace gitnstats
 {
@@ -6,7 +9,49 @@ namespace gitnstats
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git'n'Stats: Git History Analyzer");
+
+            //TODO: handle non-existant repo exception
+            using (var repo = new Repository(@"/Users/rubberduck/Documents/Source/theupsyde/"))
+            {
+                foreach (var commit in repo.Commits.Take(1))
+                {
+                    Console.Write(commit.Sha + "\t");
+                    Console.WriteLine(commit.MessageShort);
+
+                    PrintFilePathsAt(commit);
+                }
+            }
+        }
+
+        private static void PrintFilePathsAt(Commit commit)
+        {
+            foreach (var treeEntry in Flatten(commit.Tree))
+            {
+                Console.Write("\t");
+                Console.Write(treeEntry.Path + "\t");
+                Console.WriteLine(treeEntry.TargetType.ToString() + "\t");
+            }
+        }
+
+        private static IEnumerable<TreeEntry> Flatten(Tree tree)
+        {
+            foreach (var treeEntry in tree)
+            {
+                switch (treeEntry.TargetType)
+                {
+                    case TreeEntryTargetType.Blob:
+                    case TreeEntryTargetType.GitLink:
+                        yield return treeEntry;
+                        break;
+                    case TreeEntryTargetType.Tree:
+                        foreach (var innerEntry in Flatten((Tree)treeEntry.Target))
+                        {
+                            yield return innerEntry;
+                        }
+                        break;
+                }
+            }
         }
     }
 }
