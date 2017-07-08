9fcaf031f56595bafa01e063eda27d3de5fa71bf
Christopher J. McClellan <ckuhn203@gmail.com>
2017-07-08 01:07:05 -0400

    Put test project in right place

--- tests/gitnstats.tests/UnitTest1.cs
+++ tests/gitnstats.tests/UnitTest1.cs
@@ -1,14 +1,16 @@
 using System;
 using Xunit;
+using gitnstats;
+using LibGit2Sharp;
 
-namespace tests
+namespace gitnstats.tests
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-             Assert.True(true);
+
         }
     }
 }
