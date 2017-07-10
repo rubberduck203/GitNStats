4c7906893ed684da8c328866fe5b2b129d711056
Christopher J. McClellan <ckuhn203@gmail.com>
2017-07-09 23:11:45 -0400

    Visitor shouldn't know about it's children.
    This was an artifact from extracting the base class.

--- src/gitnstats/Visitor.cs
+++ src/gitnstats/Visitor.cs
@@ -4,12 +4,12 @@ namespace GitNStats
 {
     public abstract class Visitor
     {
-        public delegate void VisitedHandler(CommitVisitor visitor, Commit commit);
+        public delegate void VisitedHandler(Visitor visitor, Commit commit);
         public event VisitedHandler Visited;
         
         public abstract void Walk(Commit commit);
 
-        protected virtual void OnVisited(CommitVisitor visitor, Commit commit)
+        protected virtual void OnVisited(Visitor visitor, Commit commit)
         {
             Visited?.Invoke(visitor, commit);
         }
