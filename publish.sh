5c5cda9fa9a306c2af215380daa552e6b5ffc3fd
Christopher J. McClellan <ckuhn203@gmail.com>
2017-08-30 22:22:54 -0400

    Revert "Use IL Linker to reduce final exe size"
    This reverts commit 9570e75cc0f47879caa0df95fffcc552c596817b.
    
    Can't get a consistent build locally or on appveyor right now.

--- publish.sh
+++ publish.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-project=src/gitnstats/gitnstats.csproj
-framework=netcoreapp2.0
+framework=netcoreapp1.1
 bin=src/gitnstats/bin/Release
 
 echo "Cleaning ${bin}"
 rm -rf ${bin}/**
 
 # build the list of runtimes by parsing the *.csproj for runtime identifiers
-runtimes=($(grep '<RuntimeIdentifier>' $project | sed -e 's,.*<RuntimeIdentifier>\([^<]*\)</RuntimeIdentifier>.*,\1,g'))
+runtimes=($(grep '<RuntimeIdentifier>' src/gitnstats/gitnstats.csproj | sed -e 's,.*<RuntimeIdentifier>\([^<]*\)</RuntimeIdentifier>.*,\1,g'))
 
 for runtime in ${runtimes[@]}; do
+    echo "Restoring ${runtime}"
+    dotnet restore -r ${runtime}
+    
     echo "Packaging ${runtime}"
-    dotnet publish -c release -r ${runtime} /p:ShowLinkerSizeComparison=true $project
+    dotnet publish -c release -r ${runtime}
     
     build=${bin}/${framework}/${runtime}
     publish=${build}/publish
